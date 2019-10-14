2a12c37a5eb3f8cd89933e596ed0f54de1569409
trung <phamvantrung14111994@gmail.com>
2019-10-14 14:56:09 +0700

    ok

---
+++ src/Session4/LopHoc.java
@@ -0,0 +1,17 @@
+package Session4;
+
+import java.util.ArrayList;
+
+public class LopHoc {
+    public  int maLop;
+    public  String phongHoc;
+    public ArrayList<SinhVien> dsSinhVien ;//= new ArrayList<>();
+
+    public LopHoc(){
+        dsSinhVien = new ArrayList<>();
+    }
+
+//    public void themSinhvien(SinhVien sv){
+//        this.dsSinhvien.add(sv);
+//    }
+}
